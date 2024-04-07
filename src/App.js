abbe88541d9c72b26ec7d208c5ff0258a614dfaf
yshah43 <yshah43@asu.edu>
2024-04-07 16:45:43 -0700

    feat: complete form development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex App bg-black">
+      <div className="mx-auto h-screen">
+        <h1 className="text-purple-500 text-3xl mt-10 mb-4">
+          Fovus Coding Challenge
+        </h1>
+        <h1 className="text-purple-500 text-base mb-10">
+          Yash Shah |{" "}
+          <a
+            className="duration-300 text-purple-500 hover:text-purple-300"
+            href="mailto:yshah43@asu.edu"
+          >
+            yshah43@asu.edu
+          </a>{" "}
+          | (480) 952-2965
+        </h1>
+        <div class="mx-auto content-center align-center">
+          <form className="w-full max-w-sm">
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-100 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="inline-full-name"
+                >
+                  Text Input
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-full-name"
+                  type="text"
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-100 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="inline-password"
+                >
+                  Upload
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-password"
+                  type="file"
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3" />
+              <div className="md:w-2/3">
+                <button
+                  className=" duration-500 bg-purple-500 hover:bg-purple-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                  type="button"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
